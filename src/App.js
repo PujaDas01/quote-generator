fe44e164fc8338fbe5b75fcb7adf2442e90d737b
Puja Das <102240706+PujaDas@users.noreply.github.com>
2024-02-22 15:11:45 +0530

    Slightly changed style

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import QuoteGenerator from './components/QuoteGenerator';
 
 const App = () => {
   return (
-    <div className='app'>
+    <>
       <h1>Random Quote Generator</h1>
       <QuoteGenerator />
-    </div>
+    </>
   );
 }
 
